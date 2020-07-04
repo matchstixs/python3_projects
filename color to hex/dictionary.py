cde901da15a473811e561301fdd013ce9b83bcc5
matchstix <matt.mui15@gmail.com>
2020-07-03 20:50:03 -0400

    updated readme & included code refactor to color to hex app

--- color to hex/dictionary.py
+++ color to hex/dictionary.py
@@ -14,10 +14,21 @@ data = json.load(open('INPUT DIRECT PATH'))
 #1 & 4
 usrinput = input("Color to Hex ").lower()
 
-#2 script testing
 # print(data["greenyellow"])
 
 #3
 print(data[usrinput])
-
 ### 4 steps, 4 lines of code
+
+##REFACTORED 
+# import json
+# 
+# data = json.load(open("INPUT DIRECT PATH TO data.json"))
+# 
+# def translate(word):
+#     if word in data:
+#         return data[word]
+# 
+# word = input("color ")
+# output = translate(word)
+# print(output)
